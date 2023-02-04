8498b36f352db684db8279d37c074556e3f8879d
Vraj Desai <43074241+vrajdesai78@users.noreply.github.com>
2023-02-05 02:44:47 +0530

    5ire chain added

--- src/utils/wagmi.js
+++ src/utils/wagmi.js
@@ -3,14 +3,28 @@ import { polygonMumbai } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [polygonMumbai],
+const firechain = {
+  id: 997,
+  name: "5ire",
+  network: "5ireChain",
+  nativeCurrency: {
+    decimals: 18,
+    name: "5ire",
+    symbol: "5ire",
+  },
+  rpcUrls: {
+    default: { http: ["https://chain-node.5ire.network"] }
+  },
+  testnet: true,
+};
+
+const { chains, provider } = configureChains(
+  [firechain],
   [publicProvider()]
 );
 
 export const client = createClient({
   autoConnect: true,
   connectors: [new MetaMaskConnector({ chains })],
-  provider,
-  webSocketProvider,
+  provider
 });
