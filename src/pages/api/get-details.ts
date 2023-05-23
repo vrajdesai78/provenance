869649c8ef958d275ec19f708420be716c904211
vrajdesai78 <vrajdesai78@gmail.com>
2023-05-24 00:06:56 +0530

    Updated API

--- src/pages/api/get-details.ts
+++ src/pages/api/get-details.ts
@@ -5,38 +5,49 @@ import { CONTRACT_ADDRESS } from "../../utils/contractAddress";
 import provenanceABI from "../../contracts/provenance.json";
 
 interface product {
-    name: string;
-    description: string;
-    imageURL: string;
-    locationStatuses: [];
-    timestamp: [];
-    locationURL: [];
+  name: string;
+  description: string;
+  imageURL: string;
+  locationStatuses: string[];
+  timestamp: string[];
+  locationURL: string[];
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const id = req.query.id;
   const provider = new ethers.providers.JsonRpcProvider(
     "https://matic-mumbai.chainstacklabs.com"
   );
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider); 
-    const contractFactory = new ethers.Contract(
-        CONTRACT_ADDRESS,
-        provenanceABI,
-        wallet
-      );
-    
-      console.log("fetching productDetails");
-      const productDetails = await contractFactory.getProduct(id);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
+  const contractFactory = new ethers.Contract(
+    CONTRACT_ADDRESS,
+    provenanceABI,
+    wallet
+  );
 
-        const product: product = {
-            name: productDetails.name,
-            description: productDetails.description,
-            imageURL: productDetails.imageURL,
-            locationStatuses: productDetails.locationStatuses,
-            timestamp: productDetails.timestamp,
-            locationURL: productDetails.locationURL,
-        };
-      console.log(product);
-    
-      res.send(product)
+  console.log("fetching productDetails");
+  const productDetails = await contractFactory.getProduct(id);
+
+  const formattedTimestamps = [];
+
+  for (let i = 0; i < productDetails.timestamp.length; i++) {
+    const convertedTime = productDetails.timestamp[i];
+    const date = new Date(convertedTime * 1000).toLocaleString();// Change the format of the date object to a localized string
+    formattedTimestamps.push(date);
+  }
+
+  const product: product = {
+    name: productDetails.name,
+    description: productDetails.description,
+    imageURL: productDetails.imageURL,
+    locationStatuses: productDetails.locationStatuses,
+    timestamp: formattedTimestamps,
+    locationURL: productDetails.locationURL,
+  };
+  console.log(product);
+
+  res.send(product);
 }
