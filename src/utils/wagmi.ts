0bc6d8ad5370ffc4da16a3014f57c7c8039d7a76
vrajdesai78 <vrajdesai78@gmail.com>
2023-05-24 00:17:42 +0530

    Updated wagmi config

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -3,23 +3,8 @@ import { polygonMumbai } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
-const firechain = {
-  id: 997,
-  name: "5ire",
-  network: "5ireChain",
-  nativeCurrency: {
-    decimals: 18,
-    name: "5ire",
-    symbol: "5ire",
-  },
-  rpcUrls: {
-    default: { http: ["https://chain-node.5ire.network"] }
-  },
-  testnet: true,
-};
-
 const { chains, provider } = configureChains(
-  [firechain, polygonMumbai],
+  [polygonMumbai],
   [publicProvider()]
 );
 
